85dc0498a617cb7edaae08d469c192e795efd87c
Ryosuke1208 <miyuki.ryosuke3394@gmail.com>
2024-01-04 11:52:10 +0900

    2024/1/4

--- dp/a/main.cpp
+++ dp/a/main.cpp
@@ -2,40 +2,50 @@
 // #include <atcoder/all>
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-#define fore(i, a) for (auto &i : a)
+#define fore(i, a) for (auto& i : a)
 #define all(x) (x).begin(), (x).end()
 using namespace std;
 // using namespace atcoder;
 typedef long long ll;
 const int inf = INT_MAX / 2;
 const ll infl = 1LL << 60;
-template <class T> inline bool chmin(T &a, T b) {
-    if (a > b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-template <class T> inline bool chmax(T &a, T b) {
+template <class T>
+inline bool chmax(T& a, T b) {
     if (a < b) {
         a = b;
-        return true;
+        return 1;
     }
-    return false;
+    return 0;
 }
-
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
 using P = pair<int, int>;
 // using mint = modint998244353;
 /********************************************/
 
 /********************code********************/
-int main() {
-    int n;
-    cin >> n;
+ll h[100010];
+ll dp[100010];
 
+int main() {
+    ll n;
+    cin >> n;
+    rep(i, n) cin >> h[i];
+    rep(i, 100010) dp[i] = inf;
+    dp[0] = 0;
+    for (int i = 1; i < n; i++) {
+        chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
+        if (i > 1) chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
+    }
+    cout << dp[n - 1] << endl;
     return 0;
 }
-
 /***************thinking***************/
 /*
 
