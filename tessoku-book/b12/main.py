4f21eabaab3bcba57fe72a347eb5c77d276d75d0
Ryosuke1208 <miyuki.ryosuke3394@gmail.com>
2024-01-04 17:59:00 +0900

    2024/1/4

--- tessoku-book/b12/main.py
+++ tessoku-book/b12/main.py
@@ -1,1 +1,16 @@
+def f(x):
+    return x * x * x + x
+
+
 n = int(input())
+
+l = 0.0
+r = 100.0
+for i in range(20):
+    m = (l + r) / 2
+    val = f(m)
+    if val > n:
+        r = m
+    else:
+        l = m
+print(m)
