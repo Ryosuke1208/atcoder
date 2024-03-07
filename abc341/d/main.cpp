bbd1e024267e4f62bbec5c29a31cac28b7bae258
Ryosuke1208 <miyuki.ryosuke3394@gmail.com>
2024-03-07 13:33:16 +0900

    2024/3/7

--- abc341/d/main.cpp
+++ abc341/d/main.cpp
@@ -35,17 +35,17 @@ ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
 
 ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
 
-ll count_numbers(long long x, ll N, ll M, ll LCM) {
-    return x / N + x / M - 2 * (x / LCM);
+ll count_numbers(long long x, ll n, ll m, ll _lcm) {
+    return x / n + x / m - 2 * (x / _lcm);
 }
 
-ll find_kth_number(ll N, ll M, ll K) {
-    ll LCM = lcm(N, M);
-    long long low = 1, high = (long long)K * max(N, M);
+ll find_kth_number(ll n, ll m, ll k) {
+    ll _lcm = lcm(n, m);
+    long long low = 1, high = (long long)k * max(n, m);
 
     while (low < high) {
         long long mid = (low + high) / 2;
-        if (count_numbers(mid, N, M, LCM) < K) {
+        if (count_numbers(mid, n, m, _lcm) < k) {
             low = mid + 1;
         } else {
             high = mid;
@@ -56,10 +56,10 @@ ll find_kth_number(ll N, ll M, ll K) {
 }
 
 int main() {
-    ll N, M, K;
-    cin >> N >> M >> K;
+    ll n, m, k;
+    cin >> n >> m >> k;
 
-    ll result = find_kth_number(N, M, K);
+    ll result = find_kth_number(n, m, k);
     cout << result << endl;
 
     return 0;
